267dbb439260a2456168477693535fa41f026884
LoicZHU <zhu.loic@gmail.com>
2023-11-22 18:27:39 +0100

    3

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  process.key.INTERNAL_DB_URL, // TODO: add database url
+  process.env.INTERNAL_DB_URL, // TODO: add database url
   {
     dialect: 'postgres',
     dialectOptions: {
